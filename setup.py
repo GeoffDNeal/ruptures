2e634681dc2101fc88ed51b28e112d5022be8ba4
Geoffrey Neal <GeoffDNeal@users.noreply.github.com>
2018-07-31 12:18:12 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='ruptures',
-    version='1.0a1',
+    version='1.0p0',
     packages=find_packages(exclude=['docs', 'tests*', 'images']),
     install_requires=['numpy', 'scipy', 'matplotlib'],
     # url='ctruong.perso.math.cnrs.fr/ruptures',
